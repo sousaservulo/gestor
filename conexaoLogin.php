283aea857ac644585c4d28cc31ca8db6b7f09e27
sousaservulo <71534830+sousaservulo@users.noreply.github.com>
2021-05-12 07:26:19 -0300

    Add files via upload
    adicionando back end php

---
+++ conexaoLogin.php
@@ -0,0 +1,8 @@
+<?php
+$nomeservidor = "localhost";
+$database = "login";
+$username = "adminLogin";
+$password = "54321";
+// Create connection
+$conexao = mysqli_connect($nomeservidor, $username, $password, $database);
+?>
